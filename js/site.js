dd6290c5064da185d74168c48960e6f229db9fac
acieffe <acieffe@gmail.com>
2021-08-16 11:25:42 -0400

    Fully functional and descriptive project.

--- js/site.js
+++ js/site.js
@@ -15,14 +15,15 @@ function isValid() {
    } else if (rate < 0) {
       alert('Please enter an iterest rate zero (0) or higher.');
    } else {
-      document.getElementById('results').innerHTML = "";
       outputTotals(principle, term, rate);
    }
 }
 
+// This function calculates totals and injects them into the page
 function outputTotals(principle, term, rate) {
 
    let monthPay = (principle * (rate) / (1 - Math.pow(1 / (1 + rate), term)));
+   // calls fill table to calculate the toal interest paid
    let totalInt = fillTable(principle, term, rate, monthPay);
    let cost = parseFloat(principle) + parseFloat(totalInt);
 
@@ -31,31 +32,67 @@ function outputTotals(principle, term, rate) {
    document.getElementById('totalPrinc').innerHTML = `$${parseFloat(principle).toFixed(2)}`;
    document.getElementById('totalInt').innerHTML = `$${parseFloat(totalInt).toFixed(2)}`;
    document.getElementById('totalCost').innerHTML = `$${parseFloat(cost).toFixed(2)}`;
-
 }
 
-// create object containing each row of the table
+// This function calculates the desired table columns of data and places it in an array of objects
 function fillTable(principle, term, rate, monthPay) {
 
 	let balance = principle;
    let interest = 0;
    let totalInt = 0;
-	let mortTable = [];
+	let mortTable = []; // array to hold objects
 
-	for (let index = 1; index <= term; index++) {
-      interest = balance * rate;
-      principle = monthPay - interest;
-      mortTable[index] = {
-         month : index,
-         interest : interest,
-         principle : principle,
-         payment : monthPay,
-         totalInt : totalInt += interest,
-         balance : balance -= principle
-      }
+	for (let index = 0; index < term; index++) {
+		interest = balance * rate;
+
+		if (balance < monthPay) {
+			monthPay = balance + interest;
+		}
+
+		principle = monthPay - interest;
+
+		// create object containing each row of the table
+		mortTable[index] = {
+			month: index + 1,
+			payment: monthPay,
+			principle: principle,
+			interest: interest,
+			totalInt: (totalInt += interest),
+			balance: (balance -= principle),
+		};
 	}
-   console.log(mortTable);
+
+   // Send mortTable array to be displayed on page
+   displayTable(mortTable);
+   
+   // Returns the total interest to outputTotals function
    return totalInt;
 }
 
 // Function to output array of objects into page table
+function displayTable(mortTable) {
+   // get table from page
+   let tableBody = document.getElementById('results');
+
+   // get template row
+   let rowTemp = document.getElementById('loanTemplate');
+
+   // clear table
+   tableBody.innerHTML = "";
+
+   for (let index = 0; index < mortTable.length; index ++) {
+
+      let tableRow = document.importNode(rowTemp.content, true);
+
+      let rowCols = tableRow.querySelectorAll('td');
+
+      rowCols[0].textContent = mortTable[index].month;
+      rowCols[1].textContent = `$${parseFloat(mortTable[index].payment).toFixed(2)}`;
+      rowCols[2].textContent = `$${parseFloat(mortTable[index].principle).toFixed(2)}`;
+      rowCols[3].textContent = `$${parseFloat(mortTable[index].interest).toFixed(2)}`;
+      rowCols[4].textContent = `$${parseFloat(mortTable[index].totalInt).toFixed(2)}`;
+      rowCols[5].textContent = `$${parseFloat(mortTable[index].balance).toFixed(2)}`;
+
+      tableBody.appendChild(tableRow);
+   }
+}
